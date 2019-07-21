69d74a37980d03e42e6186244624045ed8de01a5
fkk <1091900529@qq.com>
2019-07-21 19:40:30 +0800

    基础框架

--- logisticsystem/src/main/java/com/kunluntop/logisticsystem/LogisticsystemApplication.java
+++ logisticsystem/src/main/java/com/kunluntop/logisticsystem/LogisticsystemApplication.java
@@ -2,8 +2,10 @@ package com.kunluntop.logisticsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LogisticsystemApplication {
 
     public static void main(String[] args) {
