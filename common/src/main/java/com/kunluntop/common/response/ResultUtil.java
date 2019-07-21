1903cefd29e974382199b2108e7fabf5db1cdbb8
fkk <1091900529@qq.com>
2019-07-21 20:15:50 +0800

    公共模块中统一返回值的封装

---
+++ common/src/main/java/com/kunluntop/common/response/ResultUtil.java
@@ -0,0 +1,21 @@
+package com.kunluntop.common.response;
+
+public class ResultUtil {
+
+
+    public static Result success(Object object,String msg) {
+        Result result = new Result();
+        result.setCode(0);
+        result.setMsg(msg);
+        result.setData(object);
+        return result;
+    }
+
+    public static Result error(Integer code, String msg) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        return result;
+    }
+
+}
